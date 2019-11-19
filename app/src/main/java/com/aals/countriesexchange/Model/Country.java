7e414e3033410eb76eb5d3390ad7f556fafe71dd
AALSamman <abdullah.alsaman@gmail.com>
2019-11-19 12:29:57 +0100

    Additional  Model Refactoring

---
+++ app/src/main/java/com/aals/countriesexchange/Model/Country.java
@@ -0,0 +1,293 @@
+package com.aals.countriesexchange.Model;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+@Entity(tableName = "countries")
+public class Country {
+
+    @SerializedName("area")
+    @Expose
+    private double area;
+    @SerializedName("cioc")
+    @Expose
+    private String cioc;
+    @SerializedName("flag")
+    @Expose
+    private String flag;
+    @SerializedName("gini")
+    @Expose
+    private double gini;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("latlng")
+    @Expose
+    @Ignore
+    private List<Double> latlng = null;
+    @SerializedName("region")
+    @Expose
+    private String region;
+    @SerializedName("borders")
+    @Expose
+    @Ignore
+    private List<String> borders = null;
+    @SerializedName("capital")
+    @Expose
+    private String capital;
+    @SerializedName("demonym")
+    @Expose
+    private String demonym;
+    @SerializedName("languages")
+    @Expose
+    @Ignore
+    private List<Language> languages = null;
+    @SerializedName("subregion")
+    @Expose
+    private String subregion;
+    @SerializedName("timezones")
+    @Expose
+    @Ignore
+    private List<String> timezones = null;
+    @SerializedName("alpha2Code")
+    @Expose
+    @PrimaryKey
+    @NonNull
+    private String alpha2Code;
+    @SerializedName("alpha3Code")
+    @Expose
+    private String alpha3Code;
+    @SerializedName("currencies")
+    @Expose
+    @Ignore
+    private List<Currency> currencies = null;
+    @SerializedName("nativeName")
+    @Expose
+    private String nativeName;
+    @SerializedName("population")
+    @Expose
+    private Integer population;
+    @SerializedName("numericCode")
+    @Expose
+    private String numericCode;
+    @SerializedName("altSpellings")
+    @Expose
+    @Ignore
+    private List<String> altSpellings = null;
+    @SerializedName("callingCodes")
+    @Expose
+    @Ignore
+    private List<String> callingCodes = null;
+    @SerializedName("translations")
+    @Expose
+    @Ignore
+    private Translations translations;
+    @SerializedName("regionalBlocs")
+    @Expose
+    @Ignore
+    private List<RegionalBloc> regionalBlocs = null;
+    @SerializedName("topLevelDomain")
+    @Expose
+    @Ignore
+    private List<String> topLevelDomain = null;
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public String getCioc() {
+        return cioc;
+    }
+
+    public void setCioc(String cioc) {
+        this.cioc = cioc;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+
+    public double getGini() {
+        return gini;
+    }
+
+    public void setGini(double gini) {
+        this.gini = gini;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Double> getLatlng() {
+        return latlng;
+    }
+
+    public void setLatlng(List<Double> latlng) {
+        this.latlng = latlng;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public List<String> getBorders() {
+        return borders;
+    }
+
+    public void setBorders(List<String> borders) {
+        this.borders = borders;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    public String getDemonym() {
+        return demonym;
+    }
+
+    public void setDemonym(String demonym) {
+        this.demonym = demonym;
+    }
+
+    public List<Language> getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(List<Language> languages) {
+        this.languages = languages;
+    }
+
+    public String getSubregion() {
+        return subregion;
+    }
+
+    public void setSubregion(String subregion) {
+        this.subregion = subregion;
+    }
+
+    public List<String> getTimezones() {
+        return timezones;
+    }
+
+    public void setTimezones(List<String> timezones) {
+        this.timezones = timezones;
+    }
+
+    public String getAlpha2Code() {
+        return alpha2Code;
+    }
+
+    public void setAlpha2Code(String alpha2Code) {
+        this.alpha2Code = alpha2Code;
+    }
+
+    public String getAlpha3Code() {
+        return alpha3Code;
+    }
+
+    public void setAlpha3Code(String alpha3Code) {
+        this.alpha3Code = alpha3Code;
+    }
+
+    public List<Currency> getCurrencies() {
+        return currencies;
+    }
+
+    public void setCurrencies(List<Currency> currencies) {
+        this.currencies = currencies;
+    }
+
+    public String getNativeName() {
+        return nativeName;
+    }
+
+    public void setNativeName(String nativeName) {
+        this.nativeName = nativeName;
+    }
+
+    public Integer getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Integer population) {
+        this.population = population;
+    }
+
+    public String getNumericCode() {
+        return numericCode;
+    }
+
+    public void setNumericCode(String numericCode) {
+        this.numericCode = numericCode;
+    }
+
+    public List<String> getAltSpellings() {
+        return altSpellings;
+    }
+
+    public void setAltSpellings(List<String> altSpellings) {
+        this.altSpellings = altSpellings;
+    }
+
+    public List<String> getCallingCodes() {
+        return callingCodes;
+    }
+
+    public void setCallingCodes(List<String> callingCodes) {
+        this.callingCodes = callingCodes;
+    }
+
+    public Translations getTranslations() {
+        return translations;
+    }
+
+    public void setTranslations(Translations translations) {
+        this.translations = translations;
+    }
+
+    public List<RegionalBloc> getRegionalBlocs() {
+        return regionalBlocs;
+    }
+
+    public void setRegionalBlocs(List<RegionalBloc> regionalBlocs) {
+        this.regionalBlocs = regionalBlocs;
+    }
+
+    public List<String> getTopLevelDomain() {
+        return topLevelDomain;
+    }
+
+    public void setTopLevelDomain(List<String> topLevelDomain) {
+        this.topLevelDomain = topLevelDomain;
+    }
+
+}
---
+++ app/src/main/java/com/aals/countriesexchange/Model/Translations.java
@@ -0,0 +1,119 @@
+package com.aals.countriesexchange.Model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Translations {
+
+    @SerializedName("br")
+    @Expose
+    private String br;
+    @SerializedName("de")
+    @Expose
+    private String de;
+    @SerializedName("es")
+    @Expose
+    private String es;
+    @SerializedName("fa")
+    @Expose
+    private String fa;
+    @SerializedName("fr")
+    @Expose
+    private String fr;
+    @SerializedName("hr")
+    @Expose
+    private String hr;
+    @SerializedName("it")
+    @Expose
+    private String it;
+    @SerializedName("ja")
+    @Expose
+    private String ja;
+    @SerializedName("nl")
+    @Expose
+    private String nl;
+    @SerializedName("pt")
+    @Expose
+    private String pt;
+
+    public String getBr() {
+        return br;
+    }
+
+    public void setBr(String br) {
+        this.br = br;
+    }
+
+    public String getDe() {
+        return de;
+    }
+
+    public void setDe(String de) {
+        this.de = de;
+    }
+
+    public String getEs() {
+        return es;
+    }
+
+    public void setEs(String es) {
+        this.es = es;
+    }
+
+    public String getFa() {
+        return fa;
+    }
+
+    public void setFa(String fa) {
+        this.fa = fa;
+    }
+
+    public String getFr() {
+        return fr;
+    }
+
+    public void setFr(String fr) {
+        this.fr = fr;
+    }
+
+    public String getHr() {
+        return hr;
+    }
+
+    public void setHr(String hr) {
+        this.hr = hr;
+    }
+
+    public String getIt() {
+        return it;
+    }
+
+    public void setIt(String it) {
+        this.it = it;
+    }
+
+    public String getJa() {
+        return ja;
+    }
+
+    public void setJa(String ja) {
+        this.ja = ja;
+    }
+
+    public String getNl() {
+        return nl;
+    }
+
+    public void setNl(String nl) {
+        this.nl = nl;
+    }
+
+    public String getPt() {
+        return pt;
+    }
+
+    public void setPt(String pt) {
+        this.pt = pt;
+    }
+
+}
