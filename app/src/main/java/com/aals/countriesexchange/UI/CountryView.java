605a42474d989cf27a4262e497d495f7242341cf
AALSamman <abdullah.alsaman@gmail.com>
2019-11-27 12:38:13 +0100

    Fix SplashScreen on back press Fix Back Arrow on CountryView NavBar

--- app/src/main/java/com/aals/countriesexchange/UI/CountryView.java
+++ app/src/main/java/com/aals/countriesexchange/UI/CountryView.java
@@ -3,6 +3,7 @@ package com.aals.countriesexchange.UI;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.aals.countriesexchange.Model.Country;
@@ -12,12 +13,18 @@ public class CountryView extends AppCompatActivity {
 
     private Country country;
     private TextView tv;
+    private ActionBar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_country_view);
+        toolbar = getSupportActionBar();
+        toolbar.setDisplayHomeAsUpEnabled(true);
+        toolbar.setDisplayShowHomeEnabled(true);
+
         tv = findViewById(R.id.tv_cs_name);
+
         country = (Country) getIntent().getSerializableExtra("country");
         tv.setText(country.getName());
 
